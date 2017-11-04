6caccbbc501218820b0ac8c44772f4a71a1627e8
KiWooyoung <ueong777@gmail.com>
2017-11-05 05:48:38 +0900

    호스트 추가 - mysql

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const dbPoolConfig = {
-    // host: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
