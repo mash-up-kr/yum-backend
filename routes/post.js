3c714a4df1d36f24ee6be98d18a8c4aac7866c11
KiWooyoung <ueong777@gmail.com>
2017-11-05 01:24:33 +0900

    add s3 config / add post routes

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const PostModel = require('../routes/post');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+/**
+ * 게시글 업로드
+ *
+ * Request
+ */
+router.post('/upload', (req, res, next) => {
+
+    let info = {
+
+    };
+
+    PostModel.uploadPost()
 });
+
+
+/**
+ *  Get Feed
+ *
+ *  Request
+ */
+router.get('/get_feed', (req, res, next) => {
+
+});
+
+/**
+ *  HashTag or Calorie Search
+ *
+ *  (/search?hashtag&calorie)
+ *  Request
+ *
+ */
+router.get('/search', (req, res, next) => {
+
+});
+
+
 
 module.exports = router;
---
+++ service/upload.js
@@ -0,0 +1,4 @@
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const config = require('./aws_s3_config');
