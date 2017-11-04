2a795b186eeb5c0ccc7eac1d2afcd1614a7eb69b
KiWooyoung <ueong777@gmail.com>
2017-11-04 21:59:18 +0900

    add routes  auth - sign in

--- routes/auth.js
+++ routes/auth.js
@@ -30,8 +30,25 @@ router.post('/sign_up', (req, res, next) => {
 
 /**
  *  SignIn
+ *
+ *  Request
+ *  - email
+ *  - password
+ *
+ *  Response
+ *  - message : String
+ *  - code : Number
  */
 router.post('/sign_in', (req, res, next) => {
+
+    let info = {
+        email : req.body.email,
+        password : req.body.password,
+    };
+
+    AuthModel.signIn(info, (err, result) => {
+
+    });
 
 });
 
